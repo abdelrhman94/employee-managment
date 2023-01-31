a196403936d7ccf6f69048639360ee4208dd5e63
Abdelrhman Farag <abdelrhman.s.farag@gmail.com>
2023-01-31 16:43:05 +0200

    getting data form

--- components/form.jsx
+++ components/form.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { BiPlus } from 'react-icons/bi';
+
+// state => get previous state
+// event => have the current dispatch function value
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.target.name]: event.target.value,
+  };
+};
 
 const Form = () => {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <form className='grid lg:grid-cols-2 w-4/6 gap-4'>
+    <form className='grid lg:grid-cols-2 w-4/6 gap-4' onSubmit={handleSubmit}>
       <div className='input-type'>
         <input
           type='text'
+          onChange={setFormData}
           name='firstname'
           className='border w-full px-5 py-3 focus:outline-none rounded-md'
           placeholder='FirstName'
@@ -14,7 +31,8 @@ const Form = () => {
       <div className='input-type'>
         <input
           type='text'
-          name='firstname'
+          onChange={setFormData}
+          name='lastname'
           className='border w-full px-5 py-3 focus:outline-none rounded-md'
           placeholder='LastName'
         />
@@ -22,6 +40,7 @@ const Form = () => {
       <div className='input-type'>
         <input
           type='text'
+          onChange={setFormData}
           name='email'
           className='border w-full px-5 py-3 focus:outline-none rounded-md'
           placeholder='Email'
@@ -30,6 +49,7 @@ const Form = () => {
       <div className='input-type'>
         <input
           type='text'
+          onChange={setFormData}
           name='salary'
           className='border w-full px-5 py-3 focus:outline-none rounded-md'
           placeholder='Salary'
@@ -38,6 +58,7 @@ const Form = () => {
       <div className='input-type'>
         <input
           type='date'
+          onChange={setFormData}
           name='date'
           className='border px-5 py-3 focus:outline-none rounded-md'
           placeholder='Salary'
@@ -48,6 +69,7 @@ const Form = () => {
         <div className='form-check'>
           <input
             type='radio'
+            onChange={setFormData}
             value='Active'
             id='radioDefault1'
             name='status'
@@ -60,6 +82,7 @@ const Form = () => {
         <div className='form-check'>
           <input
             type='radio'
+            onChange={setFormData}
             value='Inactive'
             id='radioDefault2'
             name='status'
@@ -72,7 +95,10 @@ const Form = () => {
       </div>
 
       <button className='flex justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500'>
-        Add
+        Add{' '}
+        <span className='px-1'>
+          <BiPlus size={24}></BiPlus>
+        </span>
       </button>
     </form>
   );
